21329b8d519a7b671603e8330b8b336e7b8f73c4
Kui Chen <kuiche@microsoft.com>
2018-03-06 17:56:06 -0800

    add another errorchecking format for 05-02-01

--- 05_blank-identifier/02_http-get_example/01_with-error-checking/main.go
+++ 05_blank-identifier/02_http-get_example/01_with-error-checking/main.go
@@ -18,4 +18,24 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s", page)
+	
+	anotherWay()
+}
+
+
+func anotherWay() {
+	var res *http.Response
+	var page []byte
+	var err error
+	if res, err = http.Get("http://www.geekwiseacademy.com/"); err != nil {
+		log.Fatal(err)
+	} else {
+		defer res.Body.Close()
+	}
+
+	if page, err = ioutil.ReadAll(res.Body); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf(string(page))
+	}
 }
